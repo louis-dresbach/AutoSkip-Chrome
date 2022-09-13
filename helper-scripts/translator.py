26f58b3f77cd1fc86d34c221d4e19e514383564c
Louis <daray@hotmail.de>
2022-09-13 21:09:21 +0200

    hid key in .env

--- helper-scripts/translator.py
+++ helper-scripts/translator.py
@@ -1,36 +1,49 @@
 #
 # Translate our english messages to other languages using DeepL
 # 
-import deepl, os, re
+import deepl, os, re, requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+load_dotenv()
 
-print ("Automatically translating messages.json files using DeepL")
+path = "../package/_locales/"
+translator = deepl.Translator(os.getenv("DEEPL_KEY"))
 
-key = "9d88a7a1-2310-d066-f115-bf9a9b376e23:fx"
-translator = deepl.Translator(key)
+def main():
+	print ("Automatically translating messages.json files using DeepL")
 
-languages = ["es", "de", "fr"];
-
-def translate(match):
-	text = match.group(2)
-	if text:
-		text = translator.translate_text(text, target_lang=lang).text
-	return match.group(1) + '"' + text + '"'
-
-path = "package/_locales/"
-if os.path.exists(path):
-	f = open(path + "en/messages.json")
+	# all available languages
+	languages = ["bg", "cs", "da", "de", "el", "es", "et", "fi", "fr", "hu", "id", "it", "ja", "lt", "lv", "nl", "pl", "pt-pt", "pt-br", "ro", "ru", "sk", "sl", "sv", "tr", "uk", "zh"];
 	
-	original = f.read()
-	for lang in languages:
-		print ("Creating translation for language: \"" + lang + "\"")
-		if not os.path.exists(path + lang):
-			os.makedirs(path + lang)
-		
-		result = re.sub('("message":\s*)"([^"]*)"', translate, original)
-		w = open(path + lang + "/messages.json", "w", encoding='utf-8')
-		w.write(result)
-		w.close()
-		
-	f.close()
+	#languages = ["de"] # just for testing
 	
-	print ("Finished")
+	if os.path.exists(path):
+		f = open(path + "en/messages.json")
+		
+		original = f.read()
+		for lang in languages:
+
+			def translate(match):
+				text = match.group(2)
+				if text:
+					if (lang == "de" or lang == "fr" or lang == "it" or lang == "es" or lang == "nl" or lang == "pl" or lang == "pt"  or lang == "ru"):
+						text = translator.translate_text(text, target_lang=lang, formality="less").text
+					else:
+						text = translator.translate_text(text, target_lang=lang).text
+				return match.group(1) + '"' + text + '"'
+				
+			print ("Creating translation for language: \"" + lang + "\"")
+			if not os.path.exists(path + lang):
+				os.makedirs(path + lang)
+			
+			result = re.sub('("message":\s*)"([^"]*)"', translate, original)
+			w = open(path + lang + "/messages.json", "w", encoding='utf-8')
+			w.write(result)
+			w.close()
+			
+		f.close()
+		
+		print ("Finished")
+
+	
+main()
